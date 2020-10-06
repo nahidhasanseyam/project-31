c2b4eb56eae790b22eb895f6711b664ac8791df1
nahidhasanseyam <nahidh22333@gmail.com>
2020-10-06 17:12:47 +0600

    trv_app

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,9 +4,9 @@ import 'package:flutter/services.dart';
 class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
- //Stream<String> get onAuthStateChanged => _firebaseAuth.authStateChanges().map(
-      //  (User user) => user?.uid,
-     // );
+ Stream<String> get onAuthStateChanged => _firebaseAuth.onAuthStateChanged.map(
+        (FirebaseUser user) => user?.uid,
+     );
 
 // Email & password sing up
 Future<String> createUserWithEmailAndPassword(String email,String password, String name) async{
@@ -14,8 +14,11 @@ Future<String> createUserWithEmailAndPassword(String email,String password, Stri
       email: email, password: password);
 
   // Update the username
-  //await updateUserName(name, authResult.user);
-  //return authResult.user.uid;
+ var userUpdateInfo = UserUpdateInfo();
+ userUpdateInfo.displayName = name;
+ await currentUser.updateProfile(userUpdateInfo);
+ await currentUser.reload();
+ return currentUser.uid;
 
 }
 
